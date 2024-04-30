3f3a98b8661a62ee734aececb8f8c49ed351c1ab
3944Realms <f256198830@hotmail.com>
2024-05-01 00:18:15 +0800

    2024-04-30 @Version Beta 0.0.0.1.2 UPDATED ModDataGeneratorHandler.java Acked-by: Owner TODO Improve dataGenerations.java

--- src/main/java/com/r3944realms/whimsy/datagen/ModDataGeneratorHandler.java
+++ src/main/java/com/r3944realms/whimsy/datagen/ModDataGeneratorHandler.java
@@ -1,8 +1,10 @@
 package com.r3944realms.whimsy.datagen;
 
 import com.r3944realms.whimsy.WhimsyMod;
+import com.r3944realms.whimsy.utils.Enum.LanguageEnum;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -20,7 +22,16 @@ public class ModDataGeneratorHandler {
         CompletableFuture<HolderLookup.Provider> HolderFolder = event.getLookupProvider();
 
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-
+        /*Language Provider ENGLISH CHINESE(SIM/TRA)*/
+        addLanguage(event, existingFileHelper, LanguageEnum.English, "en_us");
+        addLanguage(event, existingFileHelper, LanguageEnum.SimpleChinese, "zh_cn");
+        addLanguage(event, existingFileHelper, LanguageEnum.TraditionalChinese, "zh_tw");
         //Forge Part
+    }
+    private static void addLanguage(GatherDataEvent event, ExistingFileHelper helper, LanguageEnum language, String lan_regex){
+        event.getGenerator().addProvider(
+                event.includeClient(),
+                (DataProvider.Factory<ModLanguageProvider>) pOutput -> new ModLanguageProvider(pOutput, WhimsyMod.MOD_ID, lan_regex, language)
+        );
     }
 }
