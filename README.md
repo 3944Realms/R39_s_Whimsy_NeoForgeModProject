6d9d07fc1ff91dd1c1f9b34bc7c1f3e51fd75201
3944Realms <f256198830@hotmail.com>
2024-05-04 10:10:51 +0800

    2024-05-04 ADD ModCreativeTab.java UPDATED ModKeyValue.java `new constructor` Acked-by: Owner TODO Improve dataGenerations.java

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ![Whimsicality Minecraft Mod](https://github.com/3944Realms/R39_s_Whimsy_NeoForgeModProject/blob/master/src/main/resources/whimsicalityLogo.png?raw=true)
 ## Welcome to my first Formal Minecraft Project.
 
-1. 项目原则
-> 复用 > 构建
-> <br/>构建基础是熟悉源码工作原理
-1. 项目目标 
-
+1. 标准
+> 普通模型方块
+> 
+> 手持**Y轴**旋转 -pi/2 ， Y轴偏移1.5 ， 缩放0.2 
+> 
 > 
 
 **(Waiting for being completed)**
---
+++ src/main/java/com/r3944realms/whimsy/items/CreativeModeTab/ModCreativeTab.java
@@ -0,0 +1,36 @@
+package com.r3944realms.whimsy.items.CreativeModeTab;
+
+import com.r3944realms.whimsy.WhimsyMod;
+import com.r3944realms.whimsy.blocks.ModBlocksRegister;
+import com.r3944realms.whimsy.items.ModItemsRegister;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+public class ModCreativeTab {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, WhimsyMod.MOD_ID);
+    public static final String WHIMSY_TAB_STRING = "creativetab.whimsicality";
+    public static final String TEST = "test_tab";
+    public static final Supplier<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register(TEST,() -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .title(Component.translatable(WHIMSY_TAB_STRING))
+            .icon(() -> ModItemsRegister.TEST_ITEM.get().getDefaultInstance())
+            .displayItems(((pParameters, pOutput) -> {
+                ModItemsRegister.ITEM_SUPPLIER.forEach(item -> pOutput.accept(item.get()));
+            })).build());
+    public static String getCreativeMod(@NotNull String tabs) {
+        return WHIMSY_TAB_STRING + "." + tabs;
+    }
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+}
